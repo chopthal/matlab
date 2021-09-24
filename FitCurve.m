0b6cf0cbd24914de7b0028e8d32658fd8ad84f55
CDSeo <tjckdejr1@gmail.com>
2021-09-24 17:37:34 +0900

    Test Mass Transfer induced rate eq

--- FitCurve.m
+++ FitCurve.m
@@ -15,14 +15,14 @@ for i = 1:size(rawData, 2)
     
     % fcn = fit curve - raw curve
     eval(sprintf('fcnDisso{i} = %s;', fitFunc(i).Dissociation));
-    eval(sprintf('fcnAsso{i} = %s;', fitFunc(i).Association));    
+    eval(sprintf('fcnAsso{i} = %s;', fitFunc(i).Association));
     eval(sprintf('fcnDissoSqSum(i) = sum(fcnDisso{i}(%s).^2);', xVarDisso{i}));
     eval(sprintf('fcnAssoSqSum(i) = sum(fcnAsso{i}(%s).^2);', xVarAsso{i}));
     
     eval(sprintf('fcnDissoRes = fcnDisso{i}(%s);', xVarDisso{i}));
     eval(sprintf('fcnAssoRes = fcnAsso{i}(%s);', xVarAsso{i}));
     
-%     rawChi2(i) = fcnDissoSqSum(i) / size(fcnDissoRes, 1);    
+%     rawChi2(i) = fcnDissoSqSum(i) / size(fcnDissoRes, 1);
     rawChi2(i) = fcnDissoSqSum(i) / size(fcnDissoRes, 1) + fcnAssoSqSum(i) / size(fcnAssoRes, 1);
     
 end
