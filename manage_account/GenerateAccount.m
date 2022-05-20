65bb68c3efa1b2ca9ef44d41ee353fb2db4c6abb
cdseo <tjckdejr1@gmail.com>
2022-05-20 16:14:46 +0900

    Feat : 계정 생성 (signup) 기능

--- manage_account/GenerateAccount.m
+++ manage_account/GenerateAccount.m
@@ -1,19 +1,25 @@
-function GenerateAccount(id, password, tier)
+function [isGenerated, stateMsg] = GenerateAccount(id, password, tier)
 
+% isGenerated = GenerateAccount(id, password, tier)
 % id : string
 % password : string
 % tier : string
 
+isGenerated = false;
+stateMsg = '';
+
 MIN_PASSWORD_LENGTH = 5;
 MAX_PASSWORD_LENGTH = 20;
 
-if length(password) < MIN_PASSWORD_LENGTH
-    fprintf('Password should be longer than %d words\n', MIN_PASSWORD_LENGTH);
+if length(password) < MIN_PASSWORD_LENGTH    
+    isGenerated = false;
+    stateMsg = sprintf('Password should be longer than %d words\n', MIN_PASSWORD_LENGTH);
     return;
 end
 
 if length(password) > MAX_PASSWORD_LENGTH
-    fprintf('Password should be shorter than %d words\n', MAX_PASSWORD_LENGTH);
+    isGenerated = false;
+    stateMsg = sprintf('Password should be shorter than %d words\n', MAX_PASSWORD_LENGTH);
     return;
 end
 
@@ -28,13 +34,15 @@ end
 if isempty(fieldnames(Accounts))    
 end
 
-if isfield(Accounts, id)
-    disp('Exist ID');
+if isfield(Accounts, id)    
+    isGenerated = false;
+    stateMsg = 'Exist ID';
     return;
 end
 
 Accounts.(id).Password = hashedPassword;
 Accounts.(id).Tier = tier;
 save('accounts.mat', 'Accounts');
+isGenerated = true;
 
 end
