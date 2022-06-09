bb6dffa2ee5f502a88a9952fb0adab6ccc825b3e
cdseo <tjckdejr1@gmail.com>
2022-06-09 11:03:57 +0900

    Feat : input - output 일치화
    - Dummy data 추가 코드 제거
    - Stream.Close() 추가
    - Decrypt padding : Default -> None 변경

--- aes_dotnet/aes_dotnet.m
+++ aes_dotnet/aes_dotnet.m
@@ -1,3 +1,4 @@
+% For windows only
 if ~NET.isNETSupported
     disp('#NET is not supported in this system.')
     return
@@ -5,22 +6,25 @@ end
 
 import System.*;
 
-key = "00000000000000000000000000000000"; % length : 32
-iv = "0000000000000000"; % length : 16
+key = 'zoic-voi_qawe.krnkosjdiod1fjasdf'; % length : 32
+iv = 'c8xpc3iojdx!pijr'; % length : 16
 
-str = char(floor(rand(1, 20) * (double('z') - double('A') + 1) + double('a')));
+length(iv)
+
+str = char(floor(rand(1, 3948753) * (double('z') - double('A') + 1) + double('A')));
+% str = char(floor(rand(1, 58) * (126 - 32 + 1) + 32));
+% str = sprintf('cxiojaio -> \nsaodifjasodifasdfsioadjfioasjioj   asiodfjiopasdjfo iopasdjfiopasdjf  aipsdjfiopasdfjopasifj8924u9384 89 3#$#$#-> %%34984u398iojzo   '); 
 modStr = mod(length(str), 16);
 if modStr > 0
     str = [str blanks(16 - modStr)]; % Dummy strings for encryption
 end
-str = [str blanks(16)]; % Dummy strings for decryption
 disp(str);
 input = String(str);
 encrypted = AesEncrypt(input, key, iv);
-disp(encrypted);
 input = String(encrypted);
 decrypted = AesDecrypt(input, key, iv);
-disp(decrypted)
+disp(decrypted);
+strcmp(str, decrypted)
 
 
 function output = AesEncrypt(input, key, iv)
@@ -42,6 +46,8 @@ function output = AesEncrypt(input, key, iv)
     buf = msEncrypt.ToArray();    
     output = Convert.ToBase64String(buf, 0, buf.Length, Base64FormattingOptions.None);
     output = char(output);
+
+    csEncrypt.Close; msEncrypt.Close;
 end
 
 
@@ -53,7 +59,8 @@ function output = AesDecrypt(input, key, iv)
 
     aesAlg = Aes.Create();    
     aesAlg.Key = Encoding.UTF8.GetBytes(key);
-    aesAlg.IV = Encoding.UTF8.GetBytes(iv);    
+    aesAlg.IV = Encoding.UTF8.GetBytes(iv);   
+    aesAlg.Padding = PaddingMode.None;
 
     decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);    
     msDecrypt = MemoryStream();
@@ -63,6 +70,8 @@ function output = AesDecrypt(input, key, iv)
     buf = msDecrypt.ToArray();        
     tmp = Encoding.UTF8.GetString(buf);
     output = char(tmp);
+
+    csDecrypt.Close; msDecrypt.Close;
 end
 
 
